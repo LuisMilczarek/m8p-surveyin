cb68c239ee438920950d85a66e9adc13d6ad9484
Luís Milczarek <luis.milczarek@hotmail.com>
2025-08-28 10:42:12 -0300

    working

--- C94-M8P_Base.py
+++ C94-M8P_Base.py
@@ -36,11 +36,13 @@ def createCfgPrt(target,baudrate, inUBX=1,inNMEA=0, inRTCM=0, inRTCM3=1,
                  outUBX=1, outNMEA=1, outRTCM=0, outRTCM3=1) -> UBXMessage:
     msg = UBXMessage("CFG","CFG-PRT",SET, portID=target, baudRate=baudrate,
                      inUBX=inUBX, inNMEA=inNMEA, inRTCM=inRTCM, inRTCM3=inRTCM3,
-                     outUBX=outUBX, outNMEA=outNMEA, outRTCM=outRTCM, outRTCM3=outRTCM3)
+                     outUBX=outUBX, outNMEA=outNMEA, outRTCM=outRTCM, outRTCM3=outRTCM3,
+                     parity=4, charLen=3,nStopBits=0)
     return msg
 
 def createCfgMsg(msgClass, msgID, rateUART1=0, rateUART2=0,rateUSB=0,rateSPI=0) -> UBXMessage:
-    msg = UBXMessage("CFG","CFG-MSG",SET,msgClass=msgClass,msgID=msgID, rateUART1=rateUART1, rateUART2=rateUART2,rateUSB=rateUSB,rateSPI=rateSPI)
+    msg = UBXMessage("CFG","CFG-MSG",SET,msgClass=msgClass,msgID=msgID, rateUART1=rateUART1, rateUART2=rateUART2,rateUSB=rateUSB,rateSPI=rateSPI,
+                     )
     return msg
 
 def createCfgTModeMsg(rcvrMode, svinMinDur=60000, svinAccLimit=10000) -> UBXMessage:
@@ -91,46 +93,50 @@ def main()-> None:
     with Serial(port, 115200, timeout=3) as stream:
         ubr = UBXReader(stream, protfilter=NMEA_PROTOCOL | UBX_PROTOCOL)
         
+        cfgUART1 = createCfgPrt(Ports.UART1, baudrate=19200, inUBX=0,inRTCM3=0,inNMEA=0,outUBX=0,outNMEA=0,outRTCM3=1)
+        # print(cfgUART1.serialize())
+        # return
+        stream.write(cfgUART1.serialize())
+        
         cfgUSB = createCfgPrt(Ports.USB, baudrate=115200, inUBX=1,inRTCM3=0,outUBX=1,outRTCM3=1)
         stream.write(cfgUSB.serialize())
 
-        cfgUART1 = createCfgPrt(Ports.UART1, baudrate=19200, inUBX=0,inRTCM3=0,inNMEA=0,outUBX=0,outNMEA=0,outRTCM3=1)
-        stream.write(cfgUART1.serialize())
-        
+
         svinSetMSG = createCfgMsg(MSGClass.NAV, msgID=NavMSG.SVIN, rateUSB=1)
         stream.write(svinSetMSG.serialize())
 
         pvtSetMSG = createCfgMsg(MSGClass.NAV, msgID=NavMSG.PVT, rateUSB=1)
         stream.write(pvtSetMSG.serialize())
 
-        r1005SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1005, rateUSB=1)
+        r1005SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1005, rateUART1=1)
         stream.write(r1005SetMSG.serialize())
 
-        r1077SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1077, rateUSB=1)
+        r1077SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1077, rateUART1=1)
         stream.write(r1077SetMSG.serialize())
 
-        r1087SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1087, rateUSB=1)
+        r1087SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1087, rateUART1=1)
         stream.write(r1087SetMSG.serialize())
 
-        r1230SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1230, rateUSB=10)
+        r1230SetMSG = createCfgMsg(MSGClass.RTCM33, msgID=RTCM3MSG.R1230, rateUART1=10)
         stream.write(r1230SetMSG.serialize())
 
         tmodeSetMSG = createCfgTModeMsg(TMODE3FLAGS.SURVEY_IN,svinMinDur=min_dur,svinAccLimit=min_acc)
         stream.write(tmodeSetMSG.serialize())
+
 
         last = float("-inf")
         while True:
             _, parsed_data = ubr.read()
             if parsed_data is not None and not isinstance(parsed_data, NMEAMessage):
                 print(parsed_data)
-                if isinstance(parsed_data,UBXMessage) and parsed_data.msg_id == "NAV-SVIN":
-                    print("OI"*10E3)
+                # if isinstance(parsed_data,UBXMessage) and parsed_data.msg_id == "NAV-SVIN":
+                #     print("OI"*10E3)
             
-            now = perf_counter()
-            if now - last > 1:
-                poll = UBXMessage("CFG","CFG-PRT",POLL,portID=Ports.UART1)
-                stream.write(poll.serialize())
-                last = now
+            # now = perf_counter()
+            # if now - last > 1:
+            #     poll = UBXMessage("CFG","CFG-PRT",POLL,portID=Ports.UART1)
+            #     stream.write(poll.serialize())
+            #     last = now
 
 if __name__ == "__main__":
     main()
